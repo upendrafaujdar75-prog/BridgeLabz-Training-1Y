7b0ddcba13246ade465902d84bc09464c6b4a2ba
upendrafaujdar75-prog <upendrafaujdar75@gmail.com>
2026-01-17 22:57:42 +0530

    Intrestcalculator

--- Basic-Programs/IntrestCalculator.java
+++ Basic-Programs/IntrestCalculator.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class IntrestCalculator {
    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
+         Scanner sc = new Scanner(System.in);
         System.out.println("----Simple Interest Calculator---");
         System.out.println("Enter Principal :");
         int Principal=sc.nextInt();
