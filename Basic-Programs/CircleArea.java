ea8874892de8646ee8210551311c1b49f837236f
upendrafaujdar75-prog <upendrafaujdar75@gmail.com>
2026-01-17 23:00:43 +0530

    CircleArea

--- Basic-Programs/CircleArea.java
+++ Basic-Programs/CircleArea.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class CircleArea {
     public static void main(String[] args){
-        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Radius Of Circle:");
         int radius=sc.nextInt();
