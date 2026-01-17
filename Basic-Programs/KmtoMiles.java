7d9138ea314417ffa72f313a9352227fbbf9500c
upendrafaujdar75-prog <upendrafaujdar75@gmail.com>
2026-01-17 22:42:46 +0530

    KmtoMiles

--- Basic-Programs/KmtoMiles.java
+++ Basic-Programs/KmtoMiles.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class KmtoMiles {
     public static void main(String[] args) {
 
-        try (Scanner sc = new Scanner(System.in)) {
+            Scanner sc = new Scanner(System.in);
             System.out.print("Enter distance in kilometers: ");
             double km = sc.nextDouble();
 
@@ -12,4 +12,3 @@ public class KmtoMiles {
             System.out.println("Distance in miles: " + miles);
         }
     }
-}
